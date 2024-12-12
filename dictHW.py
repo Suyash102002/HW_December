c7d6ed40f65c8dc6b7c62ce0e90b710d0369d244
Suyash102002 <suyash.khedekar.comp@gmail.com>
2024-12-12 13:07:25 +0530

    first commit

---
+++ dictHW.py
@@ -0,0 +1,11 @@
+ages = {"Alice": 25, "Bob": 30, "Charlie": 35}
+print(ages)
+print(" ")
+
+capitals = {"France": "Paris", "Italy": "Rome", "Spain": "Madrid"}
+print(capitals)
+print(" ")
+
+person = {"name": "John", "age": 28, "is_student": True}
+print(person)
+print(" ")
---
+++ listHW1.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+print(" ")
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+print(" ")
+
+mixed = [1, "hello", 3.14, True]
+print(mixed)
+print(" ")
